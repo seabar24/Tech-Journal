f781cb67c27addebf0e050d1fc41768003ff9784
seabar24 <sean.barrick@mymail.champlain.edu>
2023-02-18 22:26:05 +0000

    passwordless ssh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -3,4 +3,17 @@
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+echo -n "Enter the username: "
+read username
+
+pubkeyloc=Tech-Journal/SYS265/linux/public-keys/id_rsa.pub
+
+userauthkey=/home/"$username"/.ssh/authorized_keys
+
+sudo useradd -m -d /home/"$username" -s /bin/bash "$username"
+sudo mkdir /home/"$username"/.ssh
+sudo cp $pubkeyloc $userauthkey
+sudo chmod 700 /home/"$username"/.ssh
+sudo chmod 600 /home/"$username"/.ssh/authorized_keys
+sudo chown -R "$username":"$username" /home/"$username"/.ssh
+
