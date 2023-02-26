84752877696ae0b58b073bc92808976bf615f75d
seabar24 <71162295+seabar24@users.noreply.github.com>
2023-02-25 22:00:51 -0500

    Update passwordless-ssh.sh

--- passwordless-ssh.sh
+++ passwordless-ssh.sh
@@ -8,6 +8,9 @@ sudo chown -R $user:$user /home/$user/.ssh
 sudo chmod 700 /home/$user/.ssh
 sudo chmod 600 /home/$user/.ssh/authorized_keys
 
-#Make sure you have the public key copied over from the machine you want to ssh from into this machine
-sudo cp __publickey__ /home/$user/.ssh/authorized_keys
+#Make sure you have the public key copied over from the machine you want to ssh from into this machine. 
+#And make sure it ends with .pub
+sudo cp *.pub /home/$user/.ssh/authorized_keys
 
+echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
+systemctl restart sshd
